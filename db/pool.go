3685ede5ee0f2909071b8297d1d5f6c82dff4546
Michal Zahradnicek <webarranger@gmail.com>
2022-04-19 13:11:05 +0200

    Add NewConnectionFromDb

--- db/pool.go
+++ db/pool.go
@@ -1,32 +1,41 @@
 package db
 
 import (
+	"errors"
+
 	"github.com/jmoiron/sqlx"
 	sqlg "github.com/mzahradnicek/sql-glue"
 )
 
-var pool map[string]*Connection
+var pool = make(map[string]*Connection)
+
+var ErrDbIsNil = errors.New("Database identifier is nil")
 
 func NewConnection(ident, dbtype, connString string, sqlgBuilder *sqlg.Builder) (err error) {
+	db, err := sqlx.Connect(dbtype, connString)
+
+	if err != nil {
+		return err
+	}
+
+	return NewConnectionFromDb(ident, db, sqlgBuilder)
+}
+
+func NewConnectionFromDb(ident string, db *sqlx.DB, sqlgBuilder *sqlg.Builder) (err error) {
 	if ident == "" {
 		ident = "default"
 	}
 
-	c := &Connection{}
-
-	c.db, err = sqlx.Connect(dbtype, connString)
-
-	if err != nil {
-		return
+	if db == nil {
+		return ErrDbIsNil
 	}
+
+	c := &Connection{db: db}
 
 	if sqlgBuilder != nil {
 		c.sqlg = sqlgBuilder
 	}
 
-	if pool == nil {
-		pool = make(map[string]*Connection)
-	}
 	pool[ident] = c
 
 	return
