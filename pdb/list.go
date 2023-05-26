4bdcff845fde3141eca0da18f3ced82b00cd6e49
Michal Zahradnicek <webarranger@gmail.com>
2023-05-26 12:48:28 +0200

    Fix Apply QueryOptions

--- pdb/list.go
+++ pdb/list.go
@@ -55,13 +55,14 @@ func (l *List) QueryOptions(opt QueryOptions) {
 	l.opt = &opt
 }
 
-func (l List) ApplyQueryOptions(q *sqlg.Qg) {
-	if l.opt != nil {
-		l.opt.ApplyToQuery(q)
-
-		l.Limit = l.opt.Limit
-		l.Offset = l.opt.Offset
+func (l *List) ApplyQueryOptions(q *sqlg.Qg) {
+	if l.opt == nil {
+		return
 	}
+
+	l.opt.ApplyToQuery(q)
+	l.Limit = l.opt.Limit
+	l.Offset = l.opt.Offset
 }
 
 func NewList(conn *Connection) *List {
